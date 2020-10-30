3da0a201a592d800953539fb5f42723467fcbfed
Tiru <Tiru@LAPTOP-N6IUUFII>
2020-10-30 15:36:48 +0530

    First Commit

---
+++ Altimetrik_Ancestry_Demo/src/test/java/com/altimetrik/demo/AltimetrikAncestryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltimetrikAncestryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
